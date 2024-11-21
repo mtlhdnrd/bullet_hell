41f4d72c0a44e5e0987b8a0de618c9ae7068ed28
Batyu <girmany321@gmail.com>
2024-11-21 13:05:27 +0100

    Registration implemented

--- web/api/register_user.php
+++ web/api/register_user.php
@@ -1,28 +1,18 @@
 <?php
 include_once("../config.php");
-header("Content-Type: application/json");
 // Define variables and initialize with empty values
-if(isset($_POST['username']) && isset($_POST['password']) && count($_POST) === 2) {
+if (!empty($_POST) && $_SERVER["REQUEST_METHOD"] === 'POST') {
     $username = $_POST['username'];
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-    //make_query($conn, "SELECT COUNT(username) FROM players WHERE `username` = \""
-    //    .mysqli_real_escape_string($conn,  $_GET[])."\"");
-    $result =
-        mysqli_query($conn, "INSERT INTO `players` (username)\"VALUES("
-        .mysqli_real_escape_string($conn,  $username)."\");");
-    if($result) {
-        $result =
-        mysqli_query($conn, "INSERT INTO `player_login` (username, passowrd)\"VALUES("
-        .mysqli_real_escape_string($conn,  $username).","
-        .mysqli_real_escape_string($conn, $password)."\");");
-        if($result) {
-            echo "true";
-        } else {
-            echo "false";
-        }
-    } else {
-        echo "false";
-    }
+    $password = $_POST['password'];
+    $query = "INSERT INTO `players` (username) VALUES (?);";
+    $result = $conn->prepare($query);
+    $result->bind_param("s", $username);
+    $result->execute();
+    $result = $conn->prepare( "INSERT INTO `player_login` (username, password) VALUES(?,?);");
+    $result->bind_param("ss", $username, $password);
+    $result->execute();
+    http_response_code(201);
+    $_SESSION['username'] = $username;
 } else {
-    echo "HIBA!";
+    echo "ejnye";
 }
--- web/register.php
+++ web/register.php
@@ -6,52 +6,59 @@ require_once "utils.php";
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bullet Hell</title>
-    <?php require_once("links.php");?>
+    <link rel="stylesheet" href="style.css">
+    <?php require_once("links.php"); ?>
 </head>
+
 <body>
     <div class="container mt-5">
-    <div class="row mb-5">
-        <h1 class="text-center text-black">Welcome to Bullet Hell</h1>
-    </div>
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-header">
-                    Sign up
-                </div>
-                <div class="card-body">
-                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                        <div class="form-group">
-                            <label for="username">Username</label>
-                            <input type="text" name="username" id="username" class="form-control" required>
-                            <?php echo(!empty($username_err)) ? "<p class='text-danger'>{$username_err}</p>" : ''; ?>
-                        </div>
-                        <div class="form-group">
-                            <label for="password">Password</label>
-                            <div class="input-group">
-                                <input type="password" name="password" class="form-control" id="password" required>
-                                <div class="input-group-append">
-                                    <button class="btn btn-secondary" type="button" onclick="togglePwVisibility()">
-                                        <i class="fa-solid fa-eye" id="eyeIcon"></i>
-                                    </button>
-                                </div>
+        <div class="row mb-5">
+            <h1 class="text-center text-black">Welcome to Bullet Hell</h1>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        Sign up
+                    </div>
+                    <div class="card-body">
+                        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" id="register-form">
+                            <div class="form-group">
+                                <label for="username">Username</label>
+                                <input type="text" name="username" id="username" class="form-control" required>
                             </div>
-                            <?php echo(!empty($password_err)) ? "<p class='text-danger'>{$password_err}</p>" : ''; ?>
-                        </div>
-                    </form>
-                    <button class="btn btn-primary my-3 px-4 py-2" style="background-color: green; border: none;" onclick="addNewUser()">Sign up</button>
-                </div>
-                <div class="card-footer">
-                    Already have an account? <a href="login.php">Log in my brother</a>
+                            <p class="text-danger d-none" id="username-exists">The username already exists</p>
+                            <p class="text-danger d-none" id="username-invalid-character">The username format invalid</p>
+
+
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <div class="input-group">
+                                    <input type="password" name="password" class="form-control" id="password" required>
+                                    <div class="input-group-append">
+                                        <button class="btn btn-secondary" type="button" onclick="togglePwVisibility()">
+                                            <i class="fa-solid fa-eye" id="eyeIcon"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                                <?php echo (!empty($password_err)) ? "<p class='text-danger'>{$password_err}</p>" : ''; ?>
+                            </div>
+                        </form>
+                        <button class="btn btn-primary my-3 px-4 py-2" style="background-color: green; border: none;" onclick="addNewUser()">Sign up</button>
+                    </div>
+                    <div class="card-footer">
+                        Already have an account? <a href="login.php">Log in my brother</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 </body>
+
 </html>
