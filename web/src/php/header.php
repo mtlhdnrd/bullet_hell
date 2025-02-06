2262b83bfdaf03cba6ef569c82e8b2493032b6f2
mtlhdnrd <60985445+mtlhdnrd@users.noreply.github.com>
2025-02-06 13:48:00 +0100

    added link to admin panel if you log in as a user with admin privileges

--- web/src/php/header.php
+++ web/src/php/header.php
@@ -1,39 +1,52 @@
-<?php require_once($_SERVER['DOCUMENT_ROOT'] . "/bullet_hell/web/src/php/links.php");
+<?php
+    require_once($_SERVER['DOCUMENT_ROOT'] . "/bullet_hell/web/src/php/links.php");
 ?>
 <nav class="navbar navbar-expand-lg bg-custom px-lg-5">
-  <div class="container-fluid">
-    <a class="navbar-brand me-auto" href="<?php $_SERVER['DOCUMENT_ROOT'] ?>/bullet_hell/web/index.php"><img
-        src="<?php $_SERVER['DOCUMENT_ROOT'] ?>/bullet_hell/web/src/images/logo.png" width="70" height="70" alt=""></a>
-    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasNavbarLabel"><a href="index.php"><img
-              src="<?php $_SERVER['DOCUMENT_ROOT'] ?>/bullet_hell/web/src/images/logo.png" width="70" height="70"
-              alt=""></a></h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body bg-custom">
-        <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
-          <li class="nav-item">
-            <a class="nav-link mx-lg-4 menu-element text-white fs-5" aria-current="page" href="#rolunk">the game</a>
-          </li>
-          <li class="nav-item ">
-            <a class="nav-link mx-lg-4 text-white menu-element fs-5"
-              href="<?php $_SERVER['DOCUMENT_ROOT'] ?>/bullet_hell/web/codex/">codex</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link mx-lg-4 text-white menu-element fs-5"
-              href="<?php $_SERVER['DOCUMENT_ROOT'] ?>/bullet_hell/web/leaderboard/">leaderboards</a>
-          </li>
-        </ul>
-      </div>
+    <div class="container-fluid">
+        <a class="navbar-brand me-auto" href="<?php $_SERVER['DOCUMENT_ROOT'] ?>/bullet_hell/web/index.php"><img
+                src="<?php $_SERVER['DOCUMENT_ROOT'] ?>/bullet_hell/web/src/images/logo.png" width="70" height="70"
+                alt=""></a>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasNavbarLabel"><a href="index.php"><img
+                            src="<?php $_SERVER['DOCUMENT_ROOT'] ?>/bullet_hell/web/src/images/logo.png" width="70"
+                            height="70" alt=""></a></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body bg-custom">
+                <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
+                    <li class="nav-item">
+                        <a class="nav-link mx-lg-4 menu-element text-white fs-5" aria-current="page" href="#rolunk">the
+                            game</a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link mx-lg-4 text-white menu-element fs-5"
+                            href="<?php $_SERVER['DOCUMENT_ROOT'] ?>/bullet_hell/web/codex/">codex</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link mx-lg-4 text-white menu-element fs-5"
+                            href="<?php $_SERVER['DOCUMENT_ROOT'] ?>/bullet_hell/web/leaderboard/">leaderboards</a>
+                    </li>
+                    <?php
+                    if(is_user_admin()) {
+                        echo "<li class=\"nav-item\">
+                                <a class=\"nav-link mx-lg-4 text-white menu-element fs-5\"
+                                href=\"/bullet_hell/web/admin/\">admin panel</a>
+                            </li>";
+                    }
+                    ?>
+                </ul>
+            </div>
+        </div>
+        <a href="<?php $_SERVER['DOCUMENT_ROOT'] ?>/bullet_hell/web/profile/" class="login-button fs-5"
+            id="login-username">
+            <?php echo $_SESSION['username']; ?>
+        </a>
+        <a href="<?php $_SERVER['DOCUMENT_ROOT'] ?>/bullet_hell/web/src/php/logout.php" id="logout"><i
+                class="fa-solid fa-right-from-bracket"></i></a>
+        <button class="navbar-toggler shadow-none border-0 pe-0" type="button" data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
     </div>
-    <a href="<?php $_SERVER['DOCUMENT_ROOT']?>/bullet_hell/web/profile/" class="login-button fs-5" id="login-username">
-      <?php echo $_SESSION['username']; ?>
-    </a>
-    <a href="<?php $_SERVER['DOCUMENT_ROOT']?>/bullet_hell/web/src/php/logout.php" id="logout"><i class="fa-solid fa-right-from-bracket"></i></a>
-    <button class="navbar-toggler shadow-none border-0 pe-0" type="button" data-bs-toggle="offcanvas"
-      data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-  </div>
 </nav>
--- web/src/php/utils.php
+++ web/src/php/utils.php
@@ -1,20 +1,29 @@
 <?php
 session_start();
-function make_query($conn, $sql)
-{
+require_once($_SERVER['DOCUMENT_ROOT'] . "/bullet_hell/web/src/php/config.php");
+function make_query($conn, $sql) {
     return $conn->query($sql);
 }
-function is_logged_in()
-{
+function is_logged_in() {
     return isset($_SESSION['username']);
 }
-function is_admin_logged_in()
-{
+function is_admin_logged_in() {
     return isset($_SESSION['admin_login']) && $_SESSION['admin_login'] == 'true';
 }
 
-function logout()
-{
+function logout() {
     unset($_SESSION['username']);
     unset($_SESSION['admin_login']);
+}
+
+function is_user_admin() {
+    if (isset($_SESSION['username'])) {
+        $username = $_SESSION['username'];
+        $stmt = $GLOBALS["conn"]->prepare("SELECT is_admin FROM player_login WHERE username=?;");
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $result = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $result[0]["is_admin"];
+    }
+    return false;
 }
