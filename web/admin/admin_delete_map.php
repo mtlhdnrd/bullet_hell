f8374a88770ce13df9998868fa2a218e0a80ec8c
mtdhdnrd <60985445+mtlhdnrd@users.noreply.github.com>
2025-02-07 09:55:11 +0100

    map management

---
+++ web/admin/admin_delete_map.php
@@ -0,0 +1,16 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . "/bullet_hell/web/src/php/config.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/bullet_hell/web/src/php/utils.php");
+if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+    $requestBody = file_get_contents('php://input');
+    parse_str($requestBody, $params);
+    $id = $params['id'];
+    $query = "DELETE FROM maps WHERE id = ?;";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    if ($stmt->errno) {
+        echo $stmt->error;
+    }
+    http_response_code(200);
+}
---
+++ web/admin/map_management.js
@@ -0,0 +1,51 @@
+function LoadMapTable() {
+    $.ajax({
+        type: "GET",
+        url: "admin_get_map_data.php",
+        dataType: "json",
+        success: function (data, textStatus, xhr) {
+            let tableContents = "";
+            $.each(data, function (index, map) {
+                let mapData = `
+                    <tr>
+                        <td>${map.id}</td>
+                        <td>${map.name}</td>
+                        <td>${map.description}</td>
+                        <td>${map.music_pack}</td>
+                        <td><i class="fa-solid fa-trash delete-btn" id=${map.id}></i><td>
+                    </tr>
+                    `;
+                tableContents += mapData;
+            });
+            $(".table-contents").html(tableContents);
+            $(".delete-btn").click(function () {
+                ConfirmDelete($(this).attr("id"));
+            });
+        },
+    });
+}
+
+function ConfirmDelete(id) {
+    if (confirm("Are you sure you want to delete the map?")) {
+        DeleteMap(id);
+    }
+}
+
+function DeleteMap(id) {
+    $.ajax({
+        type: "DELETE",
+        url: "admin_delete_map.php",
+        data: { id: id },
+        success: function (data, textStatus, xhr) {
+            switch (xhr.status) {
+                case 200:
+                    LoadMapTable();
+                    break;
+            }
+        },
+    });
+}
+
+$(document).ready(function () {
+    LoadMapTable();
+});
