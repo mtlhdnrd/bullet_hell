5ca44ff0744938f5cc8608d2259d103fb4c8e125
Barni05 <girmany321@gmail.com>
2025-02-13 11:27:14 +0100

    Database

--- web/codex/src/map_grid.js
+++ web/codex/src/map_grid.js
@@ -30,7 +30,7 @@ function LoadMaps() {
         dataType: "json", // Expect JSON response
         success: function (data, textStatus, xhr) {
             var container = $(".map-container"); // Select the container
-
+            console.log(data[0]);
             $.each(data, function (index, map) {
                 maps.push(
                     new Map(
@@ -43,6 +43,9 @@ function LoadMaps() {
                 );
             });
         },
+        error: function(xhr, status, error){
+            console.error(error);
+        }
     }).then((result) => {
         DisplayMaps();
     });
