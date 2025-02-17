eab783cb1fa5d6090781d8000d7cdcf39f3ecf77
Barni05 <girmany321@gmail.com>
2025-02-17 12:48:57 +0100

    font size modified

--- web/codex/characters.php
+++ web/codex/characters.php
@@ -12,7 +12,7 @@ $characterIndex = $_GET['character'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Codex</title>
+    <title>Characters</title>
     <link rel="stylesheet" href="src/style.css">
 </head>
 
