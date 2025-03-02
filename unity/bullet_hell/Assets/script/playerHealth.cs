202bcf010a8f369c2cb265e921e1826bdcf05554
flxshbxng. <nagydavid2005@gmail.com>
2025-03-02 12:13:57 +0100

    0302.1
    - mapped for gamepad (aim still missing)
    - removed crosshair (gamepad compatibility issues)
    - muzzle flash pre-version
    - fixed players wanting to equip each other (scanner didn't ignore "player" tag)
    - added "shootingpoint", "muzzleflash" gameobject to "shotgun" prefab

--- unity/bullet_hell/Assets/script/playerHealth.cs
+++ unity/bullet_hell/Assets/script/playerHealth.cs
@@ -2,23 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerHealth : MonoBehaviour {
+public class playerHealth : MonoBehaviour
+{
     public int currentHealth;
     public healthbar healthbarScript;
 
     //player gets damaged
-    public void playerDamaged(int dmg) {
-        if(currentHealth > 0) {
+    public void playerDamaged(int dmg)
+    {
+        if (currentHealth > 0)
+        {
             currentHealth -= dmg;
             healthbarScript.healthUpdate("p1", currentHealth);
-        } else { death(); }
+        }
+        else { death(); }
 
     }
 
-    public void death() {
+    public void death()
+    {
         Debug.Log(gameObject.name + "is dead");
     }
 
-    public void Update() {
+    public void Update()
+    {
     }
 }
--- unity/bullet_hell/Assets/script/weapons/shotgun.cs
+++ unity/bullet_hell/Assets/script/weapons/shotgun.cs
@@ -4,34 +4,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class shotgun : weapon {
-    public override void Fire() {
+public class shotgun : weapon
+{
+
+    [Header("bullet refs")]
+    public GameObject shootingPoint;
+    public muzzleFlash muzzleFlash;
+    public GameObject bullet;
+    public override void Fire()
+    {
         //firing happens
-        if(cooldown <= 0 && magazine > 0) {
+        if (cooldown <= 0 && magazine > 0)
+        {
             cooldown = 1 / firerate;
             magazine--;
             GetComponentInParent<playerHealth>().playerDamaged(damage);
-            GetComponent<AudioSource>().Play();
+            //GetComponent<AudioSource>().Play();
+            muzzleFlash.playAnim();
             Debug.Log("*pow*, rounds left: " + magazine);
             Debug.Log("you shot yourself, idiot, hp left:" + GetComponentInParent<playerHealth>().currentHealth);
             //firing doesn't happen
-        } else {
+        }
+        else
+        {
             //play click sound
             Debug.Log("*click*");
         }
     }
 
-    public override void AltFire() {
+    public override void AltFire()
+    {
         GameObject.Find("gameManager").GetComponent<gameManager>().initTest("ham");
     }
 
-    public override void SetValues() {
+    public override void SetValues()
+    {
         handCloseOffset = new float[] { 0, 0, 53 };
         handFarOffset = new float[] { -0.863f, -0.162f, 0 };
         weaponOffset = new float[] { -0.446f, -0.061f, 0 };
     }
 
-    private void Awake() {
+    private void Awake()
+    {
         weaponName = "shotgun";
         weaponHands = new char[] { 'a', 'a' };
         gameObject.layer = 8;
