2eec0f3898a81a2a0d1c1782fc34d4c369995f63
flxshbxng. <nagydavid2005@gmail.com>
2025-03-03 22:26:10 +0100

    0303.2
    - audio files are now .ogg
    - new music kit prefabs

--- unity/bullet_hell/Assets/script/musicAssets.cs
+++ unity/bullet_hell/Assets/script/musicAssets.cs
@@ -7,7 +7,12 @@ public class musicAssets : MonoBehaviour
     [Header("crt_head1")]
     public musicKit crt1Kit;
 
+    [Header("crt_head2")]//default
+    public musicKit crt2Kit;
+
     [Header("hellstar.plus")]
     public musicKit hellstarKit;
 
+    [Header("mute city")]
+    public musicKit muteKit;
 }
