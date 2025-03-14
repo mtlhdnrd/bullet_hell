a43d3428d51e403f0442defa3d1393aefa802b2f
mtlhdnrd <mtlhdnrd@protonmail.com>
2025-03-14 11:46:02 +0100

    http 400

--- web/profile/get_user_statistics.php
+++ web/profile/get_user_statistics.php
@@ -7,7 +7,9 @@ if (isset($_GET['username'])) {
     $stmt->bind_param("s", $username);
     $stmt->execute();
     $result = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+    header("Content-Type: application/json");
     echo json_encode($result);
 } else {
+    http_response_code(400);
     echo "USERNAME NOT SET";
 }
