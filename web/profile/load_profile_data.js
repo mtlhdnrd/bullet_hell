f8da1462158fdf7e55ea8b5ea8dc917bdedf1cd3
mtlhdnrd <mtlhdnrd@protonmail.com>
2025-03-18 11:02:51 +0100

    fixed stats in profile

--- web/profile/load_profile_data.js
+++ web/profile/load_profile_data.js
@@ -5,14 +5,12 @@ function LoadUserData(username) {
         url: "get_user_statistics.php",
         data: { username: username },
         success: function (data, textStatus, xhr) {
-            let json = $.parseJSON(data);
-            console.log(data);
-            $("#stat-points").text(json[0].points);
-            $("#stat-winrate").text(json[0].winrate);
-            $("#stat-gamesplayed").text(json[0].all_games_played);
-            $("#stat-kills").text(json[0].kills);
-            $("#stat-deaths").text(json[0].deaths);
-            $("#stat-music").text(json[0].music_pack == null ? "No Music" : json[0].music_pack);
+            $("#stat-points").text(data[0].points);
+            $("#stat-winrate").text(`${data[0].winrate}%`);
+            $("#stat-gamesplayed").text(data[0].all_games_played);
+            $("#stat-kills").text(data[0].kills);
+            $("#stat-deaths").text(data[0].deaths);
+            $("#stat-music").text(data[0].music_pack == null ? "No Music" : data[0].music_pack);
         },
     });
     return ret;
