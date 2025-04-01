70aeb31f4004064a8be11c80c88b53c09d02d762
mtlhdnrd <60985445+mtlhdnrd@users.noreply.github.com>
2025-04-01 12:58:42 +0200

    add default music and skin to player at registration

--- web/register/register.js
+++ web/register/register.js
@@ -11,11 +11,26 @@ function RegisterUser() {
                 url: "register_user.php",
                 data: { username: username, password: hash },
                 success: function (data, textStatus, xhr) {
-                    console.log(xhr.status);
                     switch (xhr.status) {
                         case 201:
                             window.open("../index.php", "_self");
                     }
+                    $.ajax({
+                        type: "POST",
+                        url: "../src/php/add_music_pack_to_player.php",
+                        data: {username: username, musicKit: "CRT_HEAD1"},
+                        success: function(data, textStatus, xhr){
+                            console.log("Default music pack added to user");
+                        }
+                    });
+                    $.ajax({
+                        type: "POST",
+                        url: "../src/php/add_skin_to_player.php",
+                        data: {username: username, skinName: "knight"},
+                        success: function(data, textStatus, xhr){
+                            console.log("Default skin added to user");
+                        }
+                    });
                 },
                 error: function (data, textStatus, xhr) {
                     console.error(xhr);
