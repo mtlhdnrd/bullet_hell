25b0ebf9ec6f0da0a832290d4c29f4198dda6800
mtlhdnrd <60985445+mtlhdnrd@users.noreply.github.com>
2025-04-14 12:59:40 +0200

    added sql transactions to multi-query operations

--- web/register/register_user.php
+++ web/register/register_user.php
@@ -9,15 +9,27 @@ if (!empty($_POST) && $_SERVER["REQUEST_METHOD"] === 'POST') {
         http_response_code(400);
         die("username already exists");
     }
+    $conn->begin_transaction();
+    $result = true;
     $query = "INSERT INTO `players` (username,points,all_wins,all_games_played,kills,deaths,music_pack_id,active_skin_id) VALUES (?,0,0,0,0,0,NULL,(SELECT `id` FROM `player_skins` WHERE `name` = 'knight'));";
-    $result = $conn->prepare($query);
-    $result->bind_param("s", $username);
-    $result->execute();
-    $result = $conn->prepare("INSERT INTO `player_login` (username, password) VALUES(?,?);");
-    $result->bind_param("ss", $username, $password);
-    $result->execute();
-    http_response_code(201);
-    $_SESSION['username'] = $username;
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("s", $username);
+    $result = $result && $stmt->execute();
+
+    $stmt = $conn->prepare("INSERT INTO `player_login` (username, password) VALUES(?,?);");
+    $stmt->bind_param("ss", $username, $password);
+    $result = $result && $stmt->execute();
+
+    if ($result) {
+        $conn->commit();
+        echo "success";
+        http_response_code(201);
+        $_SESSION['username'] = $username;
+    } else {
+        $conn->rollback();
+        echo "failed";
+        http_response_code(500);
+    }
 } else {
     http_response_code(400);
 }
